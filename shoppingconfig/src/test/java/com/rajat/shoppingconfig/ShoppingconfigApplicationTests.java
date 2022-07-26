9b11330072ac5b4fb80efc3ca5f661f1ad3f17b2
Rajat Singh <singhrp.rajat98@gmail.com>
2022-07-26 11:23:20 +0530

    First commit - added dependencies, base structure for config server

---
+++ shoppingconfig/src/test/java/com/rajat/shoppingconfig/ShoppingconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajat.shoppingconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
